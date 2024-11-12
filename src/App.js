e1f76d1f42b155a700679f02a1e85049d52517d5
ElenMuradyan <muradyanelen0701@gmail.com>
2024-11-12 21:28:03 +0400

    feat(App): create routing between pages using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+import Main from "./pages/MainPage";
+import MainLayout from "./layouts/MainLayout";
+import CabinetLayout from "./layouts/CabinetLayout";
 function App() {
   return (
     <div className="App">
+      <RouterProvider
+      router={
+        createBrowserRouter(
+          createRoutesFromElements(
+            <Route path="/" element={<MainLayout/>}>
+                <Route index element={<Login/>}></Route>
+                <Route path="/login" element={<Login/>}></Route>
+                <Route path="/register" element={<Register/>}></Route>
+              <Route path="/cabinet" element={<CabinetLayout/>}>
+                <Route path="/main" element={<Main/>}></Route>
+              </Route>
+            </Route>
+          )
+        )
+      }
+      />
     </div>
   );
 }
